d384fc9e60a0b84edd310cb9875cc02027922c6b
gs256 <51964952+gs256@users.noreply.github.com>
2026-04-05 16:52:03 +0300

    cell highlight and swap

--- src/Placeholder.tsx
+++ src/Placeholder.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { useSharedContext } from './shared-context'
 import { cn } from '@/utils/cn'
 
@@ -6,6 +6,8 @@ export function Placeholder(props: { index: number }) {
   const sharedContext = useSharedContext()
   const value = sharedContext.items.at(props.index) ?? ''
   const [iconLoading, setIconLoading] = useState(false)
+  const [highlighted, setHighlighted] = useState(false)
+  const ref = useRef(null)
 
   const isUrl = useMemo(() => {
     return Boolean(value) && value.startsWith('http') // FIXME
@@ -25,39 +27,82 @@ export function Placeholder(props: { index: number }) {
 
   const firstLetter = (url?.host.at(0) ?? '').toUpperCase()
 
-  const handleDrop = (e: React.DragEvent) => {
-    e.preventDefault()
-    const dropped = e.dataTransfer?.getData('text/plain')
-    if (Boolean(dropped) && dropped.startsWith('http')) {
-      sharedContext.setUrl(props.index, dropped)
-    }
-  }
-
   const handleClick = () => {
     if (isUrl) {
       window.open(value)
     }
   }
 
+  const handleDragEnter = () => {
+    const dragData = sharedContext.dragData
+    if (dragData && dragData.index !== props.index) {
+      setHighlighted(true)
+    }
+  }
+
+  const handleDragExit = (e: React.DragEvent) => {
+    if (e.currentTarget.contains(e.relatedTarget as Node)) return
+    console.log('exit')
+    setHighlighted(false)
+  }
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault()
+  }
+
+  const handleDragStart = (e: React.DragEvent) => {
+    e.dataTransfer.setData('text/plain', value)
+    sharedContext.setDragData({ index: props.index, value })
+  }
+
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault()
+    const dragData = sharedContext.dragData
+    if (dragData) {
+      sharedContext.swapValues(props.index, dragData.index)
+    } else {
+      const dropped = e.dataTransfer?.getData('text/plain')
+      if (Boolean(dropped) && dropped.startsWith('http')) {
+        sharedContext.setUrl(props.index, dropped)
+      }
+    }
+    sharedContext.setDragData(null)
+    setHighlighted(false)
+  }
+
   return (
     <div
       onDrop={handleDrop}
-      onDragOver={(e) => e.preventDefault()}
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragExit}
+      onDragOver={handleDragOver}
       data-tip={value}
-      className="card card-border bg-base-300 w-17 h-17 text-center items-center justify-center tooltip select-none cursor-pointer"
+      className={cn(
+        'card card-border bg-base-300 w-17 h-17 text-center items-center justify-center tooltip select-none cursor-pointer',
+        highlighted && 'bg-emerald-900',
+      )}
       onClick={handleClick}
     >
-      {isUrl ? (
-        <div className="rounded-full w-12 h-12 bg-gray-800 flex items-center justify-center">
-          <FaviconImg
-            key={faviconUrl}
-            src={faviconUrl}
-            setLoading={setIconLoading}
-          />
-          {iconLoading && <div className="absolute">{firstLetter}</div>}
+      {value && (
+        <div
+          ref={ref}
+          onDragStart={handleDragStart}
+          className="rounded-full w-12 h-12 bg-gray-800 flex items-center justify-center"
+          draggable
+        >
+          {isUrl ? (
+            <>
+              <FaviconImg
+                key={faviconUrl}
+                src={faviconUrl}
+                setLoading={setIconLoading}
+              />
+              {iconLoading && <div className="absolute">{firstLetter}</div>}
+            </>
+          ) : (
+            <>{firstLetter}</>
+          )}
         </div>
-      ) : (
-        <>{firstLetter}</>
       )}
     </div>
   )
@@ -81,7 +126,10 @@ function FaviconImg(props: {
       alt="favicon"
       onLoad={() => setStatus('loaded')}
       onError={() => setStatus('error')}
-      className={cn('w-7 h-7', status !== 'loaded' && 'invisible')}
+      className={cn(
+        'w-7 h-7 pointer-events-none',
+        status !== 'loaded' && 'invisible',
+      )}
     />
   )
 }
--- src/shared-context.ts
+++ src/shared-context.ts
@@ -2,7 +2,14 @@ import { createContext, useContext, useState } from 'react'
 
 export interface SharedState {
   items: string[]
-  setUrl: (index: number, url: string) => void
+  dragData: {
+    index: number
+    value: string
+  } | null
+
+  setUrl(index: number, url: string): void
+  setDragData(dragData: SharedState['dragData']): void
+  swapValues(index1: number, index2: number): void
 }
 
 export const SharedContext = createContext<SharedState | undefined>(undefined)
@@ -19,13 +26,29 @@ export function useSharedContext() {
 
 export function useSharedState() {
   const [items, setItems] = useState<string[]>(['', '', ''])
+  const [dragData, setDragData] = useState<SharedState['dragData'] | null>(null)
 
   function setUrl(index: number, url: string) {
     setItems(items.map((item, i) => (i === index ? url : item)))
   }
 
+  const swapValues = (index1: number, index2: number) => {
+    const value1 = items[index1]
+    const value2 = items[index2]
+    setItems(
+      items.map((item, i) => {
+        if (i === index1) return value2
+        if (i === index2) return value1
+        return item
+      }),
+    )
+  }
+
   return {
     items: items,
     setUrl,
+    dragData,
+    setDragData,
+    swapValues,
   } satisfies SharedState
 }
