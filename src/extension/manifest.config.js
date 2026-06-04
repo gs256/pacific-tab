48e40c8ec70ffdc23ec56fe8d302dee8d780fc96
gs256 <51964952+gs256@users.noreply.github.com>
2026-06-04 18:45:29 +0300

    extension overrides newtab page

--- src/extension/manifest.config.js
+++ src/extension/manifest.config.js
@@ -4,8 +4,7 @@ export default defineManifest({
   manifest_version: 3,
   name: 'Pacific Tab',
   version: '1.0.0',
-  options_page: 'index.html',
-  action: {
-    // default_popup: 'index.html',
+  chrome_url_overrides: {
+    newtab: 'index.html',
   },
 })
